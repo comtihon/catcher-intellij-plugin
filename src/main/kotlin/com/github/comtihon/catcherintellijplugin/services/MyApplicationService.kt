6b83da626ac5f6c36d600382f4550451e6899fbc
GitHub Action <action@github.com>
2020-11-09 13:25:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/comtihon/catcherintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.comtihon.catcherintellijplugin.services
+
+import com.github.comtihon.catcherintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/comtihon/catcherintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/comtihon/catcherintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.comtihon.catcherintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.comtihon.catcherintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
